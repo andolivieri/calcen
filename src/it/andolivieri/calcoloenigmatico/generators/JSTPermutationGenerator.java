efd133a3bbe0352d7fca9d5b98e6abfc2c794f85
Andrea Olivieri <andrea.olivieri@infocert.it>
2016-03-07 15:49:04 +0100

    first commit

---
+++ src/it/andolivieri/calcoloenigmatico/generators/JSTPermutationGenerator.java
@@ -0,0 +1,147 @@
+package it.andolivieri.calcoloenigmatico.generators;
+
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+import java.util.TreeSet;
+
+
+public class JSTPermutationGenerator<T>{
+
+	int[] p, pi, dir;
+	int currentMobileIxd;
+	int count, total, N;
+	boolean bounce = false;
+	List<T> elements;
+
+	public static void main(String[] args){
+
+		int n = 4;
+		
+		Set<Integer> s = new HashSet<Integer>();
+		s.add(Integer.valueOf(0));
+		s.add(Integer.valueOf(1));
+		s.add(Integer.valueOf(2));
+		s.add(Integer.valueOf(3));
+		s.add(Integer.valueOf(4));
+		
+		JSTPermutationGenerator<Integer> g = new JSTPermutationGenerator<Integer>(s);
+
+		
+		while(g.hasNext())
+			System.out.println(g.next().toString());
+		System.out.println(g.count);
+		assert g.count == factorial(n);
+	}
+
+
+	public JSTPermutationGenerator(Set<T> elems){
+
+		N = elems.size();
+		p   = new int[N];
+		pi   = new int[N];
+		dir   = new int[N];
+		elements = new ArrayList<T>(elems.size());
+		elements.addAll(elems);
+		
+		for (int i = 0; i < N; i++) {
+			dir[i] = -1;
+			p[i]  = i;
+			pi[i] = i;
+		}
+		currentMobileIxd = N - 1;
+		count = 0;
+		total = factorial(N);
+	}
+
+	public List<T> next(){
+
+		if(count > 0){
+
+			int gmi = findGreatestMobileIdx();
+			
+			if(gmi < 0)
+				return null;
+			
+			if(bounce){
+				for(int j=0; j<=N-1;j++){
+					if(p[j] > p[gmi])
+						dir[j] *= -1;
+				}
+			}
+				
+			// swap
+			int next = gmi + dir[gmi];
+			swap(p, gmi, gmi + dir[gmi]);
+			swap(dir, gmi, gmi + dir[gmi]);
+			// bounce
+			if(!isMobile(next)){
+				bounce = true;
+			}else{
+				bounce = false;
+			}
+			
+		}
+		
+		count++;
+		//debug();
+		return permute();
+
+	}
+	
+	private List<T> permute(){
+		List<T> r = new ArrayList<T>(N);
+		for(int i=0; i<p.length;i++){
+			r.add(i,elements.get(p[i]));
+		}
+		return r;
+	}
+
+	private boolean isMobile(int idx){
+		if(idx + dir[idx] < 0 || idx + dir[idx] >= N)
+			return false;
+		return p[idx] > p[idx + dir[idx]]; 
+	}
+
+	private static void swap(int[] x, int a, int b){
+		int z = x[a];
+		x[a] = x[b];
+		x[b] = z;
+	}
+
+	private int findGreatestMobileIdx() {
+		int maxValue = Integer.MIN_VALUE;
+		int maxIdx = -1;
+		for(int i=0; i<N; i++){
+			if(i + dir[i] > -1 && i + dir[i] < N)
+				if(p[i] > maxValue && p[i] > p[i + dir[i]]){
+					maxValue = p[i];
+					maxIdx = i;
+				}
+		}
+		return maxIdx;
+	}
+
+	public boolean hasNext(){
+		return count < total;
+	}
+
+	public static int factorial(int n) {
+		return n==0 ? 1 : n*factorial(n-1);
+	}
+	
+	private void debug(){
+		System.out.print(count + ": ");
+		System.out.print("[");
+		for(int i=0; i<N; i++)
+			System.out.print((dir[i] < 0 ? "<": " " )+ p[i] + (dir[i] > 0 ? ">" : " "));
+		System.out.println("]");
+	}
+
+
+
+
+
+}
---
+++ src/it/andolivieri/calcoloenigmatico/generators/KPermutationGenerator.java
@@ -0,0 +1,113 @@
+package it.andolivieri.calcoloenigmatico.generators;
+
+import java.util.ArrayList;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Set;
+
+public class KPermutationGenerator<T> {
+
+	private List<T> elements;
+	public int total, count;
+	private int N, K;
+	private int[] comb;
+	private Set<T> currentSubset;
+	private JSTPermutationGenerator<T> gen;
+	
+	public static void main(String[] args){
+		
+		Set<Integer> s = new HashSet<Integer>();
+		s.add(Integer.valueOf(0));
+		s.add(Integer.valueOf(1));
+		s.add(Integer.valueOf(2));
+		s.add(Integer.valueOf(3));
+		s.add(Integer.valueOf(4));
+		
+		KPermutationGenerator<Integer> g = new KPermutationGenerator<>(s, 3);
+		
+		while(g.hasNext())
+			System.out.println(g.next());
+		System.out.println(g.count);
+	}
+	
+	public KPermutationGenerator(Set<T> elems, int k){
+		
+		N = elems.size();
+		K = k;
+		elements = new ArrayList<T>(N);
+		elements.addAll(elems);
+		
+		comb = new int[K];
+		for(int i=0;i<k; i++)
+			comb[i] = i;
+		
+		initGen();
+		count = 0;
+		total = (fact(N) / (fact(K) * fact(N-K))) * fact(K);
+		
+	}
+	
+	private void initGen(){
+		currentSubset = new HashSet<T>();
+		for(int i=0; i<K;i++)
+			currentSubset.add(elements.get(comb[i]));
+		gen = new JSTPermutationGenerator<>(currentSubset);
+	}
+	
+	public boolean hasNext(){
+		return count < total;
+	}
+	
+	public List<T> next(){
+		
+		List<T> next = null;
+		
+		if(gen.hasNext()){
+			count ++;
+			return gen.next();
+		}else{
+			if(!nextCombination()){
+				return null;
+			}else{
+				initGen();
+				return next();
+			}
+		}
+	}
+	
+	private boolean nextCombination(){
+		
+		if(comb[K-1] < N - 1){
+			comb[K-1] += 1;
+			return true;
+		}else{
+			int j;
+			for(j=K-2;j>=0;j--){
+				if(comb[j] < N - K + j){
+					break;
+				}
+			}
+			if(j<0){
+				return false;
+			}else{
+				comb[j] += 1;
+			}
+			
+			while(j < K - 1){
+				comb[j+1] = comb[j] + 1;
+				j++;
+			}
+			
+			return true;
+			
+		}
+		
+	}
+	
+	public static int fact(int n) {
+		return n==0 ? 1 : n*fact(n-1);
+	}
+	
+	
+	
+}
